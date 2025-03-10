1a356aad5862d7a25931eff237b6b7d6e5ca83c6
jenishxadhikari <202624708+jenishadh@users.noreply.github.com>
2025-03-10 15:45:32 +0000

    chore: setup initial config
    - Add formatting & linting on pre-commit
    - Add shadcn for UI components

---
+++ prettier.config.mjs
@@ -0,0 +1,30 @@
+const config = {
+  semi: true,
+  singleQuote: false,
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "^types$",
+    "^@/env(.*)$",
+    "^@/types/(.*)$",
+    "^@/config/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "^@/styles/(.*)$",
+    "^@/app/(.*)$",
+    "^[./]",
+  ],
+  "importOrderSeparation": true,
+  "importOrderSortSpecifiers": true,
+  plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"]
+};
+
+export default config;
